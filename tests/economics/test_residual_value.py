ecb2d08bc3f3a75789ff3a5c37f8d339b4d5fe59
jonrsharpe <j.r.sharpe@gmail.com>
2014-12-13 22:22:20 +0000

    Improved test coverage of residual value

--- tests/economics/test_residual_value.py
+++ tests/economics/test_residual_value.py
@@ -1,14 +1,19 @@
+import pytest
+
 from py_wlc.economics.residual_value import ResidualValueCalculator
 
 TOLERANCE = 0.0001
 
+@pytest.fixture(scope="module")
+def calc():
+    return ResidualValueCalculator("linear")
+
 
 class TestResidualValue:
 
-    def test_linear(self):
-        calc = ResidualValueCalculator("linear")
+    def test_linear(self, calc):
         for end, val in [(0, 17000), (1, 14000), (2, 11000),
-                         (3, 8000), (4, 5000), (5, 2000)]:
+                         (3, 8000), (4, 5000), (5, 2000), (6, 2000)]:
             result = calc.calculate(17000, 5, 0, end, 2000)
             assert abs(result - val) < TOLERANCE
 
@@ -25,3 +30,13 @@ class TestResidualValue:
                          (3, 216), (4, 129.60), (5, 100)]:
             result = calc.calculate(1000, 5, 0, end, 100)
             assert abs(result - val) < TOLERANCE
+
+    def test_functions(self, calc):
+        assert calc.method == calc._method
+        assert "linear" in ResidualValueCalculator.available_methods()
+
+    def test_failure(self, calc):
+        with pytest.raises(ValueError):
+            _ = ResidualValueCalculator("this won't work")
+        with pytest.raises(ValueError):
+            calc.calculate(1000, 5, 0, -1, 100)
